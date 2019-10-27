c8e523310dad3b7d804a0276f5850740e4502a19
Eric Olson <zpalffy@gmail.com>
2019-10-27 14:54:13 -0500

    Simplify usage using stdout and single dir

--- src/main/java/com/eric/NefTransform.java
+++ src/main/java/com/eric/NefTransform.java
@@ -1,9 +1,6 @@
 package com.eric;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.ZoneId;
@@ -22,24 +19,16 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "nef", mixinStandardHelpOptions = true, version = { "nef 1.0",
-		"https://github.com/zpalffy/nef" }, description = "Utility to generate json metadata file for writing entries in directories.")
+@Command(name = "nef", mixinStandardHelpOptions = true, version = { "@|bold,magenta nef|@ 1.0",
+		"@|underline https://github.com/zpalffy/nef|@" }, description = "Utility to generate json metadata for entries.")
 public class NefTransform implements Callable<Integer> {
 
 	@Parameters(description = "The directory to work from.  Defaults to the current directory (${DEFAULT-VALUE})")
-	private Path[] paths = new Path[] { Paths.get(".") };
+	private Path dir = Paths.get(".");
 
 	@Option(names = { "--extensions",
 			"-e" }, description = "The file extensions to consider when creating entries.  Note: do not include the '.' character (default: ${DEFAULT-VALUE})")
 	private String[] extensions = new String[] { "md", "markdown", "txt" };
-
-	@Option(names = { "--file",
-			"-f" }, description = "The json file to output relative to each directory (default: ${DEFAULT-VALUE})")
-	private String fileName = "entries.json";
-
-	@Option(names = { "--charset",
-			"-c" }, description = "The character set to use when writing the output file (default: ${DEFAULT-VALUE})")
-	private Charset charset = Charset.defaultCharset();
 
 	@Option(names = { "--author",
 			"-a" }, description = "The default author to use if one is not specified for an entry.")
@@ -61,12 +50,6 @@ public class NefTransform implements Callable<Integer> {
 
 	private Gson gson;
 
-	private void writeJson(File file, Object obj) throws IOException {
-		FileWriter fw = new FileWriter(file, charset);
-		gson.toJson(obj, fw);
-		fw.close();
-	}
-
 	private void processDir(File dir, int baseDirLength) throws Exception {
 		List<Entry> entries = new ArrayList<>();
 		for (File file : FileUtils.listFiles(dir, extensions, recursive)) {
@@ -82,7 +65,7 @@ public class NefTransform implements Callable<Integer> {
 
 		if (!entries.isEmpty()) {
 			entries.sort(null); // sort by natural order, aka timestamp
-			writeJson(new File(dir, fileName), entries);
+			System.out.println(gson.toJson(entries));
 		}
 	}
 
@@ -94,9 +77,7 @@ public class NefTransform implements Callable<Integer> {
 		}
 		gson = builder.create();
 
-		for (Path path : paths) {
-			processDir(path.toFile(), path.toAbsolutePath().toString().length());
-		}
+		processDir(dir.toFile(), dir.toAbsolutePath().toString().length());
 		return 0;
 	}
 
