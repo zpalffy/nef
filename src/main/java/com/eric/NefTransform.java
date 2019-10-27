f42bd07e15a7052e57b8ec00529ed15efb53bd60
Eric Olson <zpalffy@gmail.com>
2019-10-27 12:52:50 -0500

    switch to picocli, changed some options to use default types

--- src/main/java/com/eric/NefTransform.java
+++ src/main/java/com/eric/NefTransform.java
@@ -1,138 +1,105 @@
 package com.eric;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.TimeZone;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.concurrent.Callable;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
-
-import com.beust.jcommander.Parameter;
-import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class NefTransform extends Command {
+import org.apache.commons.io.FileUtils;
 
-	@Parameter(names = { "--dir", "-d" }, description = "The directory to work from.  Defaults to the current directory.", order = 0)
-	private String dir = getWorkingDirectory();
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-	@Parameter(names = { "--extensions", "-e" }, description = "The file extensions to consider when creating entries.  Note: do not include the '.' character", order = 1)
-	private List<String> extensions = Lists.newArrayList("md", "markdown", "txt");
+@Command(name = "nef", mixinStandardHelpOptions = true, version = "nef 1.0", description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+public class NefTransform implements Callable<Integer> {
 
-	@Parameter(names = { "--author", "-a" }, description = "The default author to use if one is not specified for an entry.", order = 2)
+	@Parameters(description = "The directory to work from.  Defaults to the current directory (${DEFAULT-VALUE})")
+	private Path[] paths = new Path[] { Paths.get(".") };
+
+	@Option(names = { "--extensions",
+			"-e" }, description = "The file extensions to consider when creating entries.  Note: do not include the '.' character (default: ${DEFAULT-VALUE})")
+	private String[] extensions = new String[] { "md", "markdown", "txt" };
+
+	@Option(names = { "--file",
+			"-f" }, description = "The json file to output relative to each directory (default: ${DEFAULT-VALUE})")
+	private String fileName = "entries.json";
+
+	@Option(names = { "--charset",
+			"-c" }, description = "The character set to use when writing the output file (default: ${DEFAULT-VALUE})")
+	private Charset charset = Charset.defaultCharset();
+
+	@Option(names = { "--author",
+			"-a" }, description = "The default author to use if one is not specified for an entry.")
 	private String defaultAuthor;
 
-	@Parameter(names = { "--empty-files", "-ef" }, description = "Whether to write metadata files if they contain no values.  If false, files will be deleted if they already exist and there are no values.", order = 3)
-	private boolean emptyFiles;
-
-	@Parameter(names = { "--pretty-print", "-p" }, description = "Pretty print all json files.", order = 4)
+	@Option(names = { "--pretty-print", "-p" }, description = "Pretty print all json files.")
 	private boolean prettyPrint;
 
-	@Parameter(names = { "--tag-separator", "-t" }, description = "The character to use to separate tags.  Defaults to the space character.", order = 5)
+	@Option(names = { "--recursive", "-r" }, description = "Searches all subdirectories.")
+	private boolean recursive;
+
+	@Option(names = { "--tag-separator",
+			"-t" }, description = "The character to use to separate tags.  Defaults to the space character.")
 	private char tagSeparator = ' ';
 
-	@Parameter(names = { "--time-zone", "-tz" }, description = "The time zone to use when parsing date/times in file names", order = 6)
-	private String timezone = TimeZone.getDefault().getID();
+	@Option(names = { "--time-zone",
+			"-z" }, description = "The time zone to use when parsing date/times in file names (default: ${DEFAULT-VALUE})")
+	private ZoneId zone = ZoneId.systemDefault();
 
 	private Gson gson;
 
-	private void writeJson(File file, Object obj, boolean empty) {
-		if (empty && !emptyFiles) { // no values and we are not writing empty
-									// files, delete if exists:
-			if (file.exists()) {
-				verbose("Deleting file %s since it already exists, and we are not writing files with no values", file);
-				file.delete();
-			}
-		} else {
-			try {
-				verbose("Writing file %s", file);
-				FileWriter fw = new FileWriter(file);
-				gson.toJson(obj, fw);
-				fw.close();
-			} catch (IOException ex) {
-				err("An error occured while writing JSON to the file: " + file, ex);
-			}
-		}
-	}
-
-	@Override
-	protected String getProgramName() {
-		return "nef";
-	}
-
-	@Override
-	protected void beforeValidate() {
-		GsonBuilder builder = new GsonBuilder();
-		if (prettyPrint) {
-			builder.setPrettyPrinting();
-		}
-		gson = builder.create();
-	}
-
-	@Override
-	protected void validate(Collection<String> messages) {
-		if (extensions.isEmpty()) {
-			messages.add("At least one extension is required.");
-		}
+	private void writeJson(File file, Object obj) throws IOException {
+		FileWriter fw = new FileWriter(file, charset);
+		gson.toJson(obj, fw);
+		fw.close();
 	}
 
 	private void processDir(File dir, int baseDirLength) throws Exception {
-		Map<String, Long> authors = new TreeMap<>();
-		Map<String, Long> tags = new TreeMap<>();
-
-		List<Entry> entries = Lists.newArrayList();
-		for (File file : FileUtils.listFiles(dir, extensions.toArray(new String[0]), true)) {
+		List<Entry> entries = new ArrayList<>();
+		for (File file : FileUtils.listFiles(dir, extensions, recursive)) {
 			if (!file.isHidden()) {
-				Entry entry = Entry.parse(file, tagSeparator, TimeZone.getTimeZone(timezone), baseDirLength);
+				Entry entry = Entry.parse(file, tagSeparator, TimeZone.getTimeZone(zone), baseDirLength);
 				if (entry.getAuthor() == null && defaultAuthor != null) {
 					entry.setAuthor(defaultAuthor);
 				}
 
 				entries.add(entry);
-
-				if (entry.getAuthor() != null) {
-					authors.put(entry.getAuthor(),
-							authors.containsKey(entry.getAuthor()) ? authors.get(entry.getAuthor()) + 1 : 1);
-				}
-
-				if (entry.getTags() != null) {
-					for (String tag : entry.getTags()) {
-						tags.put(tag, tags.containsKey(tag) ? tags.get(tag) + 1 : 1);
-					}
-				}
 			}
 		}
-		entries.sort(null); // sort by natural order, aka timestamp
 
-		writeJson(new File(dir, "entries.json"), entries, entries.isEmpty());
-		writeJson(new File(dir, "authors.json"), authors, authors.isEmpty());
-		writeJson(new File(dir, "tags.json"), tags, tags.isEmpty());
-
-		TreeSet<String> dirs = new TreeSet<>();
-		for (File subdir : dir.listFiles((FileFilter) FileFilterUtils.directoryFileFilter())) {
-			processDir(subdir, baseDirLength);
-			dirs.add(subdir.getName());
+		if (!entries.isEmpty()) {
+			entries.sort(null); // sort by natural order, aka timestamp
+			writeJson(new File(dir, fileName), entries);
 		}
-		writeJson(new File(dir, "directories.json"), dirs, dirs.isEmpty());
 	}
 
 	@Override
-	protected void run() throws Exception {
-		long start = System.currentTimeMillis();
-		File f = new File(expandHomeDir(dir));
-		processDir(f, f.getAbsolutePath().length());
-		verbose("Processing took %s ms.", System.currentTimeMillis() - start);
+	public Integer call() throws Exception {
+		GsonBuilder builder = new GsonBuilder();
+		if (prettyPrint) {
+			builder.setPrettyPrinting();
+		}
+		gson = builder.create();
+
+		for (Path path : paths) {
+			processDir(path.toFile(), path.toString().length());
+		}
+		return 0;
 	}
 
-	public static void main(String[] args) {
-		Command.main(new NefTransform(), args);
+	public static void main(String... args) {
+		System.exit(new CommandLine(new NefTransform()).execute(args));
 	}
 }
